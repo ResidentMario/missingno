c0338ad1be0f3e1a9b8d6ab7d772ee6bac9feb52
Aleksey Bilogur <aleksey.bilogur@gmail.com>
2016-10-25 15:28:51 -0400

    Fix __init__

--- missingno/__init__.py
+++ missingno/__init__.py
@@ -1,5 +1,7 @@
 from .missingno import matrix
+from .missingno import bar
 from .missingno import heatmap
 from .missingno import dendrogram
+from .missingno import geoplot
 from .missingno import nullity_filter
 from .missingno import nullity_sort
