1f451129a2c5a8d867f82f96a5eac2e612ed20ce
Aleksey Bilogur <aleksey@residentmar.io>
2021-07-04 12:03:38 -0400

    Hotfix: remove import of deleted method (#143)

--- missingno/__init__.py
+++ missingno/__init__.py
@@ -2,7 +2,6 @@ from .missingno import matrix
 from .missingno import bar
 from .missingno import heatmap
 from .missingno import dendrogram
-from .missingno import geoplot
 from .missingno import nullity_filter
 from .missingno import nullity_sort
 from ._version import __version__
