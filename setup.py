08a465969a9a783f542e767c3757f3f69185d91a
Aleksey Bilogur <aleksey@residentmar.io>
2022-02-27 15:36:58 -0500

    Bump version.

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import setup
 setup(
     name='missingno',
     license='MIT License',
-    packages=['missingno'],  # this must be the same as the name above
+    packages=['missingno'],
     install_requires=['numpy', 'matplotlib', 'scipy', 'seaborn'],
     extras_require={'tests': ['pytest', 'pytest-mpl']},
     py_modules=['missingno'],
-    version='0.5.0',  # note to self: also update the one is the source!
+    version='0.5.1',  # note to self: also update the one is the source!
     description='Missing data visualization module for Python.',
     author='Aleksey Bilogur',
     author_email='aleksey.bilogur@gmail.com',
     url='https://github.com/ResidentMario/missingno',
-    download_url='https://github.com/ResidentMario/missingno/tarball/0.5.0',
+    download_url='https://github.com/ResidentMario/missingno/tarball/0.5.1',
     keywords=['data', 'data visualization', 'data analysis', 'missing data', 'data science', 'pandas', 'python',
               'jupyter'],
     classifiers=[]
